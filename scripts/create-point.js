b884bae2cf8c8485bd74cc555d0a0ac23d7c52f2
Andrew Figueiredo <andrewdw18@gmail.com>
2020-06-05 04:08:34 -0300

    Terceira Aula do NLW Finalizada.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -46,3 +46,56 @@ const getCities = (event) => {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities )
+
+
+// Itens de Coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+
+    const itemLi = event.target
+
+    //adicionar ou remover uma class com JS
+    itemLi.classList.toggle("selected")
+    
+
+    const itemId = event.target.dataset.id
+
+    
+    //verificar se existem itens selecioandos, se sim
+    // pegar os itens selecionados.
+    
+    //findIndex = procure até ser true
+    const alreadySelected = selectedItems.findIndex( (item) => item == itemId )
+
+
+    // Se já estiver selecionado
+    if (alreadySelected >= 0) {
+        //Tirar da seleção
+        const filteredItems = selectedItems.filter(item => item != itemId)
+
+        selectedItems = filteredItems
+    } else {
+        //se não estiver selecionado
+        // Add a seleção
+        selectedItems.push(itemId)
+
+    }
+    
+    //atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+
+
+
+} 
